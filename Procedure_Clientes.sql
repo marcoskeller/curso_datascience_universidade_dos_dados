e5a7013ae929e6f8448fe8def4fe395679956fd6
Marcos Keller <marcoskeller@hotmail.com>
2024-07-29 10:48:41 -0700

    Esse foi o primeiro commit desse projeto

---
+++ Procedure_Clientes.sql
@@ -0,0 +1,39 @@
+USE DS_TRAINNING
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		Marcos Keller
+-- Create date: 2024/07/24
+-- Description:	Procedure que cria a tabela WRK_TRANSACOES
+-- =============================================
+
+CREATE PROCEDURE [dob].[SP_WRK_TRANSACOES] 
+AS
+BEGIN
+
+-- =============================================
+--CRIACAO DA TABELA
+-- =============================================
+
+CREATE TABLE WRK_CLIENTES
+(
+
+	   [Order ID]			INT IDENTITY
+      ,[Order Date]			DATE
+      ,[Customer ID]		VARCHAR(100)
+      ,[Region]				CHAR(1)
+      ,[Rep]				VARCHAR(100)
+      ,[Item]				VARCHAR(100)
+      ,[Units]				VARCHAR(100)
+      ,[Unit Price]			FLOAT
+)
+
+
+
+END
+GO
